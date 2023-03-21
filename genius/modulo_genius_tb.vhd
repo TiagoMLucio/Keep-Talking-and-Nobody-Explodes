81c61e88dc6dca34992f4bc68a7d77ec85f4d880
TiagoMLucio <tiagolucio100@gmail.com>
2023-03-21 01:36:19 -0300

    :sparkles: feat: genius without random colors

---
+++ genius/modulo_genius_tb.vhd
@@ -0,0 +1,143 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use std.textio.all;
+
+-- entidade do testbench
+entity modulo_genius_tb is
+end entity;
+
+architecture tb of modulo_genius_tb is
+
+  -- Componente a ser testado (Device Under Test -- DUT)
+  component modulo_genius
+  port (
+    clock       : in std_logic;
+    reset       : in std_logic;
+    iniciar     : in std_logic;
+    botoes      : in std_logic_vector (3 downto 0);
+    tem_vogal   : in std_logic;
+    erros       : in std_logic_vector(1 downto 0);
+
+    pronto      : out std_logic;
+    errou       : out std_logic;
+    leds        : out std_logic_vector (3 downto 0);
+    db_clock    : out std_logic;
+    db_igual    : out std_logic;
+    db_contagem : out std_logic_vector (6 downto 0);
+    db_memoria  : out std_logic_vector (6 downto 0);
+    db_jogada   : out std_logic_vector (6 downto 0);
+    db_rodada   : out std_logic_vector (6 downto 0);
+    db_estado   : out std_logic_vector (6 downto 0)
+);
+  end component;
+
+  ---- Declaracao de sinais de entrada para conectar o componente
+  signal clk_in       : std_logic                     := '0';
+  signal rst_in       : std_logic                     := '0';
+  signal iniciar_in   : std_logic                     := '0';
+  signal botoes_in    : std_logic_vector(3 downto 0)  := "0000";
+  signal tem_vogal_in : std_logic                     := '0';
+  signal erros_in     : std_logic_vector(1 downto 0)  := "00";
+
+  ---- Declaracao dos sinais de saida
+  signal pronto_out   : std_logic                    := '0';
+  signal errou_out    : std_logic                    := '0';
+  signal leds_out     : std_logic_vector(3 downto 0) := "0000";
+  signal clock_out    : std_logic                    := '0';
+  signal igual_out    : std_logic                    := '0';
+  signal contagem_out : std_logic_vector(6 downto 0) := "0000000";
+  signal memoria_out  : std_logic_vector(6 downto 0) := "0000000";
+  signal jogada_out   : std_logic_vector(6 downto 0) := "0000000";
+  signal rodada_out   : std_logic_vector(6 downto 0) := "0000000";
+  signal estado_out   : std_logic_vector(6 downto 0) := "0000000";
+
+  -- Configurações do clock
+  signal keep_simulating : std_logic := '0';   -- delimita o tempo de geração do clock
+  constant clockPeriod   : time      := 20 ns; -- frequencia 50MHz
+
+begin
+  -- Gerador de clock: executa enquanto 'keep_simulating = 1', com o período especificado. 
+  -- Quando keep_simulating=0, clock é interrompido, bem como a simulação de eventos
+  clk_in <= (not clk_in) and keep_simulating after clockPeriod/2;
+
+  ---- DUT para Simulacao
+  dut : modulo_genius
+  port map
+  (
+    clock       => clk_in,
+    reset       => rst_in,
+    iniciar     => iniciar_in,
+    botoes      => botoes_in,
+    tem_vogal   => tem_vogal_in,
+    erros       => erros_in,
+    pronto      => pronto_out,
+    errou       => errou_out,
+    leds        => leds_out,
+    db_clock    => clock_out,
+    db_igual    => igual_out,
+    db_contagem => contagem_out,
+    db_memoria  => memoria_out,
+    db_jogada   => jogada_out,
+    db_rodada   => rodada_out,
+    db_estado   => estado_out
+  );
+
+  ---- Gera sinais de estimulo para a simulacao
+  -- Cenario de Teste : jogo como vencedor
+  process
+    type pattern_array is array (integer range <>) of std_logic_vector(3 downto 0);
+    constant jogadas : pattern_array (0 to 3) := (
+    "0010",
+    "1000",
+    "0100",
+    "0001");
+
+  begin
+    report "BOT" severity note;
+
+    -- inicio da simulacao
+    assert false report "inicio da simulacao" severity note;
+    keep_simulating <= '1'; -- inicia geracao do sinal de clock
+
+    -- gera pulso de reset (1 periodo de clock)
+    rst_in <= '1';
+    wait for clockPeriod;
+    rst_in <= '0';
+    -- pulso do sinal de Iniciar (muda na borda de descida do clock)
+    wait until falling_edge(clk_in);
+    iniciar_in <= '1';
+    wait until falling_edge(clk_in);
+    iniciar_in <= '0';
+
+    -- espera para inicio dos testes
+    wait for 1000 * clockPeriod;
+    wait until falling_edge(clk_in);
+
+    -- Cenario de Teste - acerta todas as jogadas de todas as rodadas por dois jogos consecutivos
+    
+    -- Primeiro Jogo
+    loop_rodadas1 : for r in 0 to 3 loop
+
+      loop_jogadas1 : for j in 0 to r loop
+	      report HT & "indices (j/r): " & integer'image(j) & "/" & integer'image(r) & LF;
+        report "memoria: " & integer'image(to_integer(unsigned(jogadas(j)))) & LF;
+        botoes_in <= jogadas(j);
+        wait for 10 * clockPeriod;
+        botoes_in <= "0000";
+ 	      wait for 10 * clockPeriod;
+      end loop loop_jogadas1;
+
+      wait for 3000 * clockPeriod;
+
+    end loop loop_rodadas1;
+
+    wait for 10 * clockPeriod;
+
+    ---- final do testbench
+    report "EOF" severity note;
+    keep_simulating <= '0';
+
+    wait; -- fim da simulação: processo aguarda indefinidamente
+  end process;
+end architecture;
--- genius/unidade_controle.vhd
+++ genius/unidade_controle.vhd
@@ -11,154 +11,158 @@
 -- 
 --------------------------------------------------------------------
 --
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
-ENTITY unidade_controle IS
-    PORT (
-        clock : IN STD_LOGIC;
-        reset : IN STD_LOGIC;
-        iniciar : IN STD_LOGIC;
-        meioT : IN STD_LOGIC;
-        fimT : IN STD_LOGIC;
-        fimL : IN STD_LOGIC;
-        jogada_feita : IN STD_LOGIC;
-        enderecoIgualRodada : IN STD_LOGIC;
-        jogada_correta : IN STD_LOGIC;
-        leds_mem : OUT STD_LOGIC;
-        contaE : OUT STD_LOGIC;
-        contaT : OUT STD_LOGIC;
-        contaCR : OUT STD_LOGIC;
-        registraRC : OUT STD_LOGIC;
-        zeraE : OUT STD_LOGIC;
-        zeraT : OUT STD_LOGIC;
-        zeraCR : OUT STD_LOGIC;
-        limpaRC : OUT STD_LOGIC;
-        pronto : OUT STD_LOGIC;
-        errou : OUT STD_LOGIC;
-        db_estado : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+entity unidade_controle is
+    port (
+        clock               : in std_logic;
+        reset               : in std_logic;
+        iniciar             : in std_logic;
+        meioT               : in std_logic;
+        fimT                : in std_logic;
+        fimL                : in std_logic;
+        jogada_feita        : in std_logic;
+        enderecoIgualRodada : in std_logic;
+        jogada_correta      : in std_logic;
+        leds_mem            : out std_logic;
+        contaE              : out std_logic;
+        contaT              : out std_logic;
+        contaCR             : out std_logic;
+        registraRC          : out std_logic;
+        zeraE               : out std_logic;
+        zeraT               : out std_logic;
+        zeraCR              : out std_logic;
+        limpaRC             : out std_logic;
+        pronto              : out std_logic;
+        errou               : out std_logic;
+        db_estado           : out std_logic_vector(3 downto 0)
     );
-END ENTITY;
+end entity;
 
-ARCHITECTURE fsm OF unidade_controle IS
-    TYPE t_estado IS (inicial, preparacao, espera_led, mostra_led, proximo_led,
+architecture fsm of unidade_controle is
+    type t_estado is (inicial, preparacao, espera_led, mostra_led, proximo_led,
         compara_led, inicio_rod, compara_jog, proxima_jog, espera_jog, registra,
         errou_jog, ultima_rod, proxima_rod, fim_acertou);
 
-    SIGNAL Eatual, Eprox : t_estado;
-BEGIN
+    signal Eatual, Eprox : t_estado;
+begin
 
     -- memoria de estado
-    PROCESS (clock, reset)
-    BEGIN
-        IF reset = '1' THEN
+    process (clock, reset)
+    begin
+        if reset = '1' then
             Eatual <= inicial;
-        ELSIF clock'event AND clock = '1' THEN
+        elsif clock'event and clock = '1' then
             Eatual <= Eprox;
-        END IF;
-    END PROCESS;
+        end if;
+    end process;
 
     -- logica de proximo estado
     Eprox <=
-        inicial WHEN (reset = '1') OR
-        (Eatual = inicial AND iniciar = '0') ELSE
+        inicial when (reset = '1') or
+        (Eatual = inicial and iniciar = '0') else
 
-        preparacao WHEN Eatual = inicial AND iniciar = '1' ELSE
+        preparacao when Eatual = inicial and iniciar = '1' else
 
-        espera_led WHEN (Eatual = preparacao) OR
-        (Eatual = compara_led AND enderecoIgualRodada = '1') OR
-        (Eatual = errou_jog) OR
-        (Eatual = proxima_rod) ELSE
+        espera_led when (Eatual = preparacao) or
+        (Eatual = compara_led and enderecoIgualRodada = '1') or
+        (Eatual = errou_jog) or
+        (Eatual = proxima_rod) else
 
-        mostra_led WHEN (Eatual = espera_led AND fimT = '1') OR
-        (Eatual = compara_led AND enderecoIgualRodada = '0') ELSE
+        mostra_led when (Eatual = espera_led and fimT = '1') or
+        (Eatual = proximo_led) else
 
-        proximo_led WHEN (Eatual = mostra_led AND meioT = '1') ELSE
+        proximo_led when (Eatual = compara_led and enderecoIgualRodada = '0') else
 
-        compara_led WHEN (Eatual = proximo_led) ELSE
+        compara_led when (Eatual = mostra_led and meioT = '1') else
 
-        inicio_rod WHEN (Eatual = espera_led OR Eatual = mostra_led OR
-        Eatual = proximo_led OR Eatual = compara_led) AND jogada_feita = '1' ELSE
+        inicio_rod when (Eatual = espera_led or Eatual = mostra_led or
+        Eatual = proximo_led or Eatual = compara_led) and jogada_feita = '1' else
 
-        compara_jog WHEN (Eatual = inicio_rod) OR
-        (Eatual = registra) ELSE
+        compara_jog when (Eatual = inicio_rod) or
+        (Eatual = registra) else
 
-        proxima_jog WHEN (Eatual = compara_jog AND jogada_correta = '1' AND
-        enderecoIgualRodada = '0') ELSE
+        proxima_jog when (Eatual = compara_jog and jogada_correta = '1' and
+        enderecoIgualRodada = '0') else
 
-        espera_jog WHEN (Eatual = proxima_jog) ELSE
+        espera_jog when (Eatual = proxima_jog) else
 
-        registra WHEN (Eatual = espera_jog AND jogada_feita = '1') ELSE
+        registra when (Eatual = espera_jog and jogada_feita = '1') else
 
-        errou_jog WHEN (Eatual = compara_jog AND jogada_correta = '0') ELSE
+        errou_jog when (Eatual = compara_jog and jogada_correta = '0') else
 
-        ultima_rod WHEN (Eatual = compara_jog AND jogada_correta = '1' AND
-        enderecoIgualRodada = '1') ELSE
+        ultima_rod when (Eatual = compara_jog and jogada_correta = '1' and
+        enderecoIgualRodada = '1') else
 
-        proxima_rod WHEN (Eatual = ultima_rod AND fimL = '0') ELSE
+        proxima_rod when (Eatual = ultima_rod and fimL = '0') else
 
-        fim_acertou WHEN (Eatual = ultima_rod AND fimL = '1') ELSE
+        fim_acertou when (Eatual = ultima_rod and fimL = '1') else
 
         Eatual;
 
     -- logica de saída (maquina de Moore)
-    WITH Eatual SELECT
-        zeraCR <= '1' WHEN preparacao,
-        '0' WHEN OTHERS;
+    with Eatual select
+        zeraCR <= '1' when preparacao,
+        '0' when others;
 
-    WITH Eatual SELECT
-        limpaRC <= '1' WHEN inicial,
-        '0' WHEN OTHERS;
+    with Eatual select
+        limpaRC <= '1' when inicial,
+        '0' when others;
 
-    WITH Eatual SELECT
-        zeraE <= '1' WHEN espera_led | inicio_rod,
-        '0' WHEN OTHERS;
+    with Eatual select
+        zeraE <= '1' when espera_led | inicio_rod,
+        '0' when others;
 
-    WITH Eatual SELECT
-        zeraT <= '1' WHEN proximo_led,
-        '0' WHEN OTHERS;
+    with Eatual select
+        zeraT <= '1' when proximo_led,
+        '0' when others;
 
-    WITH Eatual SELECT
-        registraRC <= '1' WHEN inicio_rod | registra,
-        '0' WHEN OTHERS;
+    with Eatual select
+        registraRC <= '1' when inicio_rod | registra,
+        '0' when others;
 
-    WITH Eatual SELECT
-        contaT <= '1' WHEN espera_led | mostra_led,
-        '0' WHEN OTHERS;
+    with Eatual select
+        contaT <= '1' when espera_led | mostra_led,
+        '0' when others;
 
-    WITH Eatual SELECT
-        contaE <= '1' WHEN proximo_led | proxima_jog,
-        '0' WHEN OTHERS;
+    with Eatual select
+        contaE <= '1' when proximo_led | proxima_jog,
+        '0' when others;
 
-    WITH Eatual SELECT
-        contaCR <= '1' WHEN proxima_rod,
-        '0' WHEN OTHERS;
+    with Eatual select
+        contaCR <= '1' when proxima_rod,
+        '0' when others;
 
-    WITH Eatual SELECT
-        leds_mem <= '1' WHEN mostra_led,
-        '0' WHEN OTHERS;
+    with Eatual select
+        leds_mem <= '1' when mostra_led,
+        '0' when others;
 
-    WITH Eatual SELECT
-        errou <= '1' WHEN errou_jog,
-        '0' WHEN OTHERS;
+    with Eatual select
+        errou <= '1' when errou_jog,
+        '0' when others;
+
+    with Eatual select
+        pronto <= '1' when fim_acertou,
+        '0' when others;
 
     -- saida de depuracao (db_estado)
-    WITH Eatual SELECT
-        db_estado <= "0000" WHEN inicial, -- 0
-        "0001" WHEN preparacao, -- 1
-        "0010" WHEN espera_led, -- 2
-        "0011" WHEN mostra_led, -- 3
-        "0100" WHEN proximo_led, -- 4
-        "0101" WHEN compara_led, -- 5
-        "0110" WHEN inicio_rod, -- 6
-        "0111" WHEN compara_jog, -- 7
-        "1000" WHEN proxima_jog, -- 8
-        "1001" WHEN espera_jog, -- 9
-        "1010" WHEN registra, -- A
-        "1011" WHEN errou_jog, -- B
-        "1100" WHEN ultima_rod, -- C
-        "1101" WHEN proxima_rod, -- D
-        "1110" WHEN fim_acertou, -- E
-        "1111" WHEN OTHERS; -- F 
+    with Eatual select
+        db_estado <= "0000" when inicial, -- 0
+        "0001" when preparacao,           -- 1
+        "0010" when espera_led,           -- 2
+        "0011" when mostra_led,           -- 3
+        "0100" when proximo_led,          -- 4
+        "0101" when compara_led,          -- 5
+        "0110" when inicio_rod,           -- 6
+        "0111" when compara_jog,          -- 7
+        "1000" when proxima_jog,          -- 8
+        "1001" when espera_jog,           -- 9
+        "1010" when registra,             -- A
+        "1011" when errou_jog,            -- B
+        "1100" when ultima_rod,           -- C
+        "1101" when proxima_rod,          -- D
+        "1110" when fim_acertou,          -- E
+        "1111" when others;               -- F 
 
-END ARCHITECTURE fsm;
+end architecture fsm;
